b452daebc734660c08e16b829cf3c7601cf0970c
Siddhesh Dongare <dongaresiddhesh3724@gmail.com>
2024-09-29 20:08:49 +0530

    Added Chat section

--- app/api/add-url/route.ts
+++ app/api/add-url/route.ts
@@ -1,75 +1,72 @@
-import { getUserCoins } from "@/actions/userDetailsAction";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { summarySchema } from "@/validations/summaryValidations";
 import vine, { errors } from "@vinejs/vine";
-import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
-import { Document } from "@langchain/core/documents";
+import { getToken } from "next-auth/jwt";
 import prisma from "@/lib/db.config";
+import { Document } from "@langchain/core/documents";
+import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
+import { getUserCoins } from "@/actions/userDetailsAction";
 
 export async function POST(req: NextRequest) {
+  const token = await getToken({ req });
+  if (!token) {
+    return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
+  }
   try {
-    const token = await getToken({ req });
-    if (!token) {
-      return NextResponse.json(
-        { message: "Unauthorised User" },
-        { status: 401 }
-      );
-    }
     const body = await req.json();
     const validator = vine.compile(summarySchema);
-
     const payload = await validator.validate(body);
-    const userCoins = await getUserCoins(payload.user_id);
-    if (userCoins === null || (userCoins?.coins && userCoins?.coins < 10)) {
+
+    // * Check if user has sufficient coins or not
+    const userConis = await getUserCoins(payload.user_id);
+    if (userConis === null || (userConis?.coins && userConis.coins < 10)) {
       return NextResponse.json(
         {
-          message: "You dont have sufficient coins. Please add more coins",
+          message:
+            "You don't have sufficient coins for summary.Please add your coins.",
         },
         { status: 400 }
       );
     }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
     let text: Document<Record<string, any>>[];
     try {
-      const loader = YoutubeLoader.createFromUrl(payload.url, {
+      const loader = YoutubeLoader.createFromUrl(payload.url!, {
         language: "en",
         addVideoInfo: true,
       });
-
       text = await loader.load();
     } catch (error) {
       return NextResponse.json(
-        { message: "No transscript found for this video. try another video" },
+        {
+          message:
+            "No Transcript available for this video.Plese try another video",
+        },
         { status: 404 }
       );
     }
 
-    const summary = await prisma.summary.create({
+    const chat = await prisma.summary.create({
       data: {
         ...payload,
         user_id: Number(payload.user_id),
-        title: text[0].metadata?.title ?? "404 Title not found",
+        title: text[0].metadata?.title ?? "No Title found!",
       },
     });
     return NextResponse.json({
-      message: "URL added successfully",
-      data: summary,
+      message: "Url Added Successfully!",
+      data: chat,
     });
   } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
       return NextResponse.json(
-        {
-          message: "Please check the validations",
-          errors: error.message,
-        },
+        { message: "Please check validation errors", errors: error.messages },
         { status: 422 }
       );
     }
     return NextResponse.json(
-      {
-        message: "Something went wrong. Please try again",
-      },
+      { message: "Something went wrong.Please try again!" },
       { status: 500 }
     );
   }
